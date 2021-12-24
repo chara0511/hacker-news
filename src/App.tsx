3a74233d6303f276acc44b6d6191ade260a1f4a6
Juan Carlos Chara <escor05_11@hotmail.com>
2021-12-24 18:59:53 -0500

    chore: Add and config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import * as React from 'react'
+
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = React.useState(0)
 
   return (
     <div className="App">

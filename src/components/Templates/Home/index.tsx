bc599c7e90de6e652b94e59244c786c8b4544ae4
Juan Carlos Chara <escor05_11@hotmail.com>
2021-12-25 01:24:09 -0500

    fix: Manage errors returning fallback using error boundary

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 
-import { Footer, Header, Main } from '../../Organisms'
+import { ErrorFallback, Footer, Header, Main } from '../../Organisms'
 
 const Home = () => {
   return (
-    <>
+    <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => window.location.reload()}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </ErrorBoundary>
   )
 }
 

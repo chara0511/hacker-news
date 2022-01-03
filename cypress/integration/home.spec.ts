74dfe46ea556f59cb0f9d2e36da7dd36bcf2f5a3
Juan Carlos Chara <escor05_11@hotmail.com>
2022-01-03 07:27:54 -0500

    chore: Change directives in main.yml

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -3,32 +3,24 @@ const newsApiUrl = `${Cypress.env(
   'newsApiUrl'
 )}/search_by_date?query=angular&page=0`
 
-describe(
-  'Home page',
-  {
-    env: {
-      baseUrl: 'http://localhost:3000'
-    }
-  },
-  () => {
-    beforeEach(() => {
-      cy.intercept('GET', newsApiUrl).as('getNews')
-      cy.visit(`${Cypress.env('baseUrl')}`)
-      cy.getBySelectorLike('skeleton-0').should('be.visible')
-      cy.wait('@getNews').then(({ response }: any) => {
-        expect(response.statusCode).to.eq(200)
-        assert.isNotNull(response.body, 'getNews API call has data')
+describe('Home page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', newsApiUrl).as('getNews')
+    cy.visit('/')
+    cy.getBySelectorLike('skeleton-0').should('be.visible')
+    cy.wait('@getNews').then(({ response }: any) => {
+      expect(response.statusCode).to.eq(200)
+      assert.isNotNull(response.body, 'getNews API call has data')
+    })
+  })
+  it('display hits', () => {
+    cy.getBySelector('skeleton-0').should('not.exist')
+    cy.getBySelector('hits')
+      .children()
+      .each(($li) => {
+        cy.wrap($li)
       })
-    })
-    it('display hits', () => {
-      cy.getBySelector('skeleton-0').should('not.exist')
-      cy.getBySelector('hits')
-        .children()
-        .each(($li) => {
-          cy.wrap($li)
-        })
-    })
-  }
-)
+  })
+})
 
 export {}

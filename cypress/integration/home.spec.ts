74ddf5d49b6c9530a3417ca0a9513d3a153cb1df
Juan Carlos Chara <escor05_11@hotmail.com>
2022-01-03 06:22:07 -0500

    chore: Add baseurl in home.spec.ts

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -3,24 +3,32 @@ const newsApiUrl = `${Cypress.env(
   'newsApiUrl'
 )}/search_by_date?query=angular&page=0`
 
-describe('Home page', () => {
-  beforeEach(() => {
-    cy.intercept('GET', newsApiUrl).as('getNews')
-    cy.visit('/')
-    cy.getBySelectorLike('skeleton-0').should('be.visible')
-    cy.wait('@getNews').then(({ response }: any) => {
-      expect(response.statusCode).to.eq(200)
-      assert.isNotNull(response.body, 'getNews API call has data')
-    })
-  })
-  it('display hits', () => {
-    cy.getBySelector('skeleton-0').should('not.exist')
-    cy.getBySelector('hits')
-      .children()
-      .each(($li) => {
-        cy.wrap($li)
+describe(
+  'Home page',
+  {
+    env: {
+      baseUrl: 'http://localhost:3000'
+    }
+  },
+  () => {
+    beforeEach(() => {
+      cy.intercept('GET', newsApiUrl).as('getNews')
+      cy.visit(`${Cypress.env('baseUrl')}`)
+      cy.getBySelectorLike('skeleton-0').should('be.visible')
+      cy.wait('@getNews').then(({ response }: any) => {
+        expect(response.statusCode).to.eq(200)
+        assert.isNotNull(response.body, 'getNews API call has data')
       })
-  })
-})
+    })
+    it('display hits', () => {
+      cy.getBySelector('skeleton-0').should('not.exist')
+      cy.getBySelector('hits')
+        .children()
+        .each(($li) => {
+          cy.wrap($li)
+        })
+    })
+  }
+)
 
 export {}

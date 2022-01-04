9e0c4037e46bfb3d4e2b55de4cd1969d22c9b58c
Juan Carlos Chara <escor05_11@hotmail.com>
2022-01-04 02:27:05 -0500

    chore: Enable video in tests

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const newsApiUrl = 'https://hn.algolia.com/api/v1//search_by_date?query=angular&page=0'
+// const newsApiUrl = `${Cypress.env('newsApiUrl')}/search_by_date?query=angular&page=0`
 
 describe('Home page', () => {
   beforeEach(() => {
+    // cy.intercept('GET', newsApiUrl).as('getNews')
     cy.visit('/')
-    cy.intercept(newsApiUrl).as('getNews')
-    // cy.getBySelectorLike('skeleton-0').should('be.visible')
-    cy.wait('@getNews').then(({ response }: any) => {
-      expect(response.statusCode).to.eq(200)
-      assert.isNotNull(response.body, 'getNews API call has data')
-    })
+    cy.getBySelectorLike('skeleton-0').should('be.visible')
+    // cy.wait('@getNews').then(({ response }: any) => {
+    //   expect(response.statusCode).to.eq(200)
+    //   assert.isNotNull(response.body, 'getNews API call has data')
+    // })
   })
   it('display hits', () => {
-    // cy.getBySelector('skeleton-0').should('not.exist')
+    cy.getBySelector('skeleton-0').should('not.exist')
     cy.getBySelector('hits')
       .children()
       .each(($li) => {
